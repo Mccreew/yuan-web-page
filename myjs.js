a09f400e714fec27cd558f2c13a162181140bf11
傅俊玮 <fujunwei2162@yundasys.com>
2023-07-24 16:32:08 +0800

    1

--- myjs.js
+++ myjs.js
@@ -1,23 +1,40 @@
-var x = 0;
+var bgImgContainer = document.getElementsByClassName('news-bg-img-container')[0]
+
+function viewImg(index) {
+    var imgList = bgImgContainer.children
+    for (var i = 0; i < imgList.length; i++) {
+        if (i == index) {
+            imgList[i].style.display = ""
+            continue
+        }
+        imgList[i].style.display = "none"
+    }
+}
+
 
 //点击什么，就用getElementById获取那个被点击的元素
 //再给它一个function()，让它每次被点击的时候，都去执行里面的代码。
 document.getElementById("three-1").onclick = function () {
-    console.log("第一个div点击了")
-    document.getElementById("news-bg-container").style.backgroundImage="url('imgs/1.jpeg')"
+    viewImg(0)
 }
 document.getElementById("three-2").onclick = function () {
-    console.log("第二个div点击了")
-    document.getElementById("news-bg-container").style.backgroundImage="url('imgs/2.jpeg')"
+    viewImg(1)
 }
 document.getElementById("three-3").onclick = function () {
-    console.log("第三个div点击了")
-    document.getElementById("news-bg-container").style.backgroundImage="url('imgs/3.jpeg')"
-
+    viewImg(2)
 }
 document.getElementById("three-4").onclick = function () {
-    console.log("第四个div点击了")
-    document.getElementById("news-bg-container").style.backgroundImage="url('imgs/4.jpeg')"
+    viewImg(3)
 }
+
+var lastImgIndex = 0
+
+setInterval(() => {
+    viewImg(lastImgIndex++)
+    if(lastImgIndex>=bgImgContainer.children.length - 1){
+        lastImgIndex = 0
+    }
+}, 5000);
+
 
 
